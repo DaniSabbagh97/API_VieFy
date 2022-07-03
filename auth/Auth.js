d9c13aa374fa4aede73d174aa5d6faa085ebbd72
AitorG <tormentilla92@gmail.com>
2022-07-03 21:21:36 +0200

    prettier auth

--- auth/Auth.js
+++ auth/Auth.js
@@ -3,31 +3,33 @@ const jwt = require('jsonwebtoken')
 module.exports = (UserModel, config) => {
   class Auth {
     /**
-    * Creates new JWT from user already validated
-    * @param {{email: string, password: string}} user 
-    */
+     * Creates new JWT from user already validated
+     * @param {{email: string, password: string}} user
+     */
     async getToken(user) {
       try {
-        const encryptedPassword = await encrypter(user.contrasenia)//Desencripta la contraseña
+        const encryptedPassword = await encrypter(user.contrasenia) //Desencripta la contraseña
         const dbUser = await UserModel.findOne({
-          where: { email: user.email, contrasenia: encryptedPassword },//Busca en la BBDD el usuario y la contrasñea encriptado
+          where: { email: user.email, contrasenia: encryptedPassword }, //Busca en la BBDD el usuario y la contrasñea encriptado
           attributes: {
-            exclude: ['contrasenia']
+            exclude: ['contrasenia'],
           },
-          raw: true // para que devuelva un usuario plano sin que sea una instancia
+          raw: true, // para que devuelva un usuario plano sin que sea una instancia
         })
-        
-        if (dbUser) { // validated
-          const tokenUser = {...dbUser}
+
+        if (dbUser) {
+          // validated
+          const tokenUser = { ...dbUser }
           delete tokenUser.pdf
           delete tokenUser.imagen
-          const token = jwt.sign(tokenUser, config.secretKey, { expiresIn: config.tokenExpireSeconds })//elegir a donde va desde el login en la API
+          const token = jwt.sign(tokenUser, config.secretKey, { expiresIn: config.tokenExpireSeconds }) //elegir a donde va desde el login en la API
           console.log(token)
-          return { token, dbUser }//TODO ENVIAR OTRA COSA
-        } else { // not validated
+          return { token, dbUser } //TODO ENVIAR OTRA COSA
+        } else {
+          // not validated
           throw new Error('Usuario o contraseña inválidos')
         }
-      } catch(e) {
+      } catch (e) {
         throw e
       }
     }
--- auth/encriptString.js
+++ auth/encriptString.js
@@ -2,10 +2,7 @@ const crypto = require('crypto')
 
 module.exports = (string) => {
   return new Promise((resolve, reject) => {
-    const encrypted = crypto
-      .createHash('sha256')
-      .update(string)
-      .digest('hex')
+    const encrypted = crypto.createHash('sha256').update(string).digest('hex')
     resolve(encrypted)
   })
 }
